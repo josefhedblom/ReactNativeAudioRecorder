1220f455150b037647be059e7be44c3d39d9e23d
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-28 20:11:40 +0200

    Add router to index

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express   = require('express');
 const cors      = require('cors');
 const connectDB = require('./config/db');
+const RecordingRoutes = require('./routes/recordings');
 const app       = express();
 
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 connectDB();
+
+app.use(RecordingRoutes)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is started on port: ${PORT}`));
