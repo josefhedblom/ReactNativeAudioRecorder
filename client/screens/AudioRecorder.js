dd4d22920b861a4d7fb973566070266ae69641af
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-28 22:53:17 +0200

    Add functions for record, play, pause, save and stop

--- client/screens/AudioRecorder.js
+++ client/screens/AudioRecorder.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 export default function AudioRecorder() {
+
+    async function startRecording(){}
+    async function stopRecording(){}
+    async function saveRecording(){}
+    async function playRecording(){}
+    async function pauseRecording(){}
     return (
         <View>
             <Text></Text>
