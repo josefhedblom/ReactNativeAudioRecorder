5f149fc82ae5a642017ae1e74952b8d3b541a305
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-28 22:47:11 +0200

    Add Navigation and Bottom tabs

--- client/App.js
+++ client/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AudioPlayer   from './screens/AudioPlayer'
 import AudioRecorder from './screens/AudioRecorder'
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Record">
+          <Tab.Screen name="Record" component={AudioRecorder} />
+          <Tab.Screen name="Playlist" component={AudioPlayer} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
