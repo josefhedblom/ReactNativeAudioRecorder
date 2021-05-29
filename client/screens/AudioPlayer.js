477040f2691db5bf26c97006a38a2d0ceccb3566
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-29 19:36:49 +0200

    Import useState and add state for recordings and sound

--- client/screens/AudioPlayer.js
+++ client/screens/AudioPlayer.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, FlatList } from 'react-native'
 import { Audio } from 'expo-av';
 export default function AudioPlayer() {
 
+    const [recordnings, setRecordings] = useState([]);
+    const [sound,            setSound] = useState();
+    
     showRecordings = ({}) => {}
     async function playRecordning(){}
     async function pauseRecordning(){}
