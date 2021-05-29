059566215fcc0cb44d67f762060839998060dd78
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-29 19:41:47 +0200

    Import axios and add get request for recordings

--- client/screens/AudioPlayer.js
+++ client/screens/AudioPlayer.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, FlatList } from 'react-native'
 import { Audio } from 'expo-av';
+import axios from 'axios';
 export default function AudioPlayer() {
 
     const [recordnings, setRecordings] = useState([]);
     const [sound,            setSound] = useState();
 
-    useEffect(() => {})
+    useEffect(() => {
+        axios.get('http://192.168.1.31:6000/')
+        .then(data => setRecordings(data.data.recordnings))
+        .catch(error => console.log(error.message));
+    })
 
     showRecordings = ({}) => {}
     async function playRecordning(){}
